932c0586a45ae157066d9bbb3daa1014f065de14
al-aminbhuyan <alaminbhuyan321@gmail.com>
2020-03-30 09:59:52 +0600

    some codding in project7

--- project7/Blog/homeapp/views.py
+++ project7/Blog/homeapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Contact
 
@@ -13,8 +13,12 @@ def contact(request):
         email = request.POST['email']
         content = request.POST['content']
         #print(name, email, phone, content)
-        contact = Contact( name = name, email = email, phone= phone, content= content)
-        contact.save()
+        if len(name)<2 or len(phone)<11 or len(email)<4 or len(content)<5:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact = Contact( name = name, email = email, phone= phone, content= content)
+            contact.save()
+            messages.success(request,"Your message is Successfully send")
     return render(request,'homeapp/contact.html')
 def about(request):
     return render(request, 'homeapp/about.html')
